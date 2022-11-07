b2f349289c0b14d8f69138666469b9090af58c44
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-11-07 13:38:47 -0600

    Update header.php

--- header.php
+++ header.php
@@ -6,16 +6,25 @@
     <title>Homework 4</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     
-    <nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="index.php">My Database</a>
-    </div>
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="instructors.php">Instructors</a></li>
-      <li><a href="courses.php">Courses</a></li>
-      <li><a href="sections.php">Sections</a></li>
-      <li><a href="projects.php">Projects</a></li>
+ <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="index.php">My Database</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="instructors.php">Instructors <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="courses.php">Courses</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="sections.php">Sections</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="projects.php">Projects</a>
+      </li>
     </ul>
   </div>
 </nav>
