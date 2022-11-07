cfddf7fffdd0a82a577a28483ebacaeb8bdb8389
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-11-07 13:46:04 -0600

    Create projects.php

---
+++ projects.php
@@ -0,0 +1,49 @@
+<?php require_once("header.php"); ?>
+
+<body>
+    <h1>Projects</h1>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>ProjectID</th>
+      <th>Name</th>
+      <th>SectionID</th>
+    </tr>
+  </thead>
+  
+  <tbody>
+    <?php
+$servername = "localhost:3306";
+$username = "tamrined_suser";
+$password = "(_y)XTDI)NmV";
+$dbname = "tamrined_4013Homework3";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT ProjectID, SectionID, Name FROM Section";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  while($row = $result->fetch_assoc()) {
+?>
+  <tr>
+    <td><?=$row["ProjectID"]?></td>
+    <td><?=$row["Name"]?></td>
+    <td><?=$row["SectionID"]?></td>
+  </tr>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+  </tbody>
+    </table>
+
+    <?php require_once("footer.php"); ?>
