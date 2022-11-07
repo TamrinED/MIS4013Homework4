5e6d3bfe254cd9f0b375b59a06dbcdf53eaee89d
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-11-07 12:57:58 -0600

    Update courses.php

--- courses.php
+++ courses.php
@@ -25,16 +25,10 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-//$ciid = $_POST['InstructorID'];      
-//echo $iid;
-$sql = "SELECT CourseID, Prefix, Number
-FROM Course;
-//WHERE c.InstructorID=i.InstructorID"
-//echo $sql;
-    $result = $conn->query($sql);
+$sql = "SELECT CourseID, Prefix, Number FROM Course;
+$result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-  // output data of each row
   while($row = $result->fetch_assoc()) {
 ?>
   <tr>
