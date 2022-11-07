89b400f21f95126acebf1ee7d96b1b6b55a76aeb
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-11-07 12:36:41 -0600

    Update header.php

--- header.php
+++ header.php
@@ -5,4 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Homework 4</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  <nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="index.php">Database Connection</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="instructors.php">Instructors</a></li>
+      <li><a href="related.php">Related</a></li>
+    </ul>
+  </div>
+</nav>
   </head>
