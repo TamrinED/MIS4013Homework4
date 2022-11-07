26280364d4606e61b56838d31fdd963109979e14
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-11-07 13:43:11 -0600

    Update sections.php

--- sections.php
+++ sections.php
@@ -1,1 +1,49 @@
+<?php require_once("header.php"); ?>
 
+<body>
+    <h1>Sections</h1>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>SectionID</th>
+      <th>Prefix</th>
+      <th>Number</th>
+    </tr>
+  </thead>
+  
+  <tbody>
+    <?php
+$servername = "localhost:3306";
+$username = "tamrined_suser";
+$password = "(_y)XTDI)NmV";
+$dbname = "tamrined_4013Homework3";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT SectionID, Prefix, Number FROM Section";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  while($row = $result->fetch_assoc()) {
+?>
+  <tr>
+    <td><?=$row["SectionID"]?></td>
+    <td><?=$row["Prefix"]?></td>
+    <td><?=$row["Number"]?></td>
+  </tr>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+  </tbody>
+    </table>
+
+    <?php require_once("footer.php"); ?>
