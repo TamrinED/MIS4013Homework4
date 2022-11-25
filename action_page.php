cd8fcaef9ffd3fab3a4793979ae809b32ba93455
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-11-25 16:00:02 -0600

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -1,21 +1,21 @@
 <?php require_once("header.php"); ?>
 
 if <?php ($_POST["projects"] === 'biz') {
-  echo "<h2>Business Projects:</h2>
+  echo <h2>Business Projects:</h2>
 
 <ul>
   <li>Sweaters</li>
   <li>Waterbottle</li>
-</ul>"; ?>
+</ul>; ?>
 } elseif <?php ($_POST["projects"] === 'infosys') {
-  echo "<h2>Information Systems Projects:</h2>
+  echo <h2>Information Systems Projects:</h2>
 
 <ul>
   <li>Espian</li>
   <li>Cabins In Broken Bow</li>
   <li>Website</li>
   <li>Database Connection</li>
-</ul> "; ?>
+</ul> ; ?>
 }
 
 
