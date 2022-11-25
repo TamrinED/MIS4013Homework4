fccc3ed90de506e32453f1e22ff5bf7a2b7ec4b4
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-11-25 15:03:17 -0600

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -1,26 +1,26 @@
 <?php require_once("header.php"); ?>
 
-if (projects == biz) {
-echo
-<h2>Business Projects:</h2>
+<?php
+$p = projects;
+
+if ($p == "biz") {
+  echo "<h2>Business Projects:</h2>
 
 <ul>
   <li>Sweaters</li>
   <li>Waterbottle</li>
-</ul> 
-}
-
-else if (projects == infosys) {
-echo
-<h2>Information Systems Projects:</h2>
+</ul>";
+} elseif ($p == "infosys") {
+  echo "<h2>Information Systems Projects:</h2>
 
 <ul>
   <li>Espian</li>
   <li>Cabins In Broken Bow</li>
   <li>Website</li>
   <li>Database Connection</li>
-</ul> 
+</ul> ";
 }
+?>
 
 
 <?php require_once("footer.php"); ?>
