8462c890ed6a301d7192b27fdd8c66a1ab5bb588
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-11-25 12:29:00 -0600

    Update instructors.php

--- instructors.php
+++ instructors.php
@@ -114,8 +114,40 @@ if ($result->num_rows > 0) {
 }
 $conn->close();
 ?>
+
   </tbody>
     </table>
+
+<br/>
+      <!-- Button trigger modal -->
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addInstructor">
+        Add New
+      </button>
+
+      <!-- Modal -->
+      <div class="modal fade" id="addInstructor" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="addInstructorLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="addInstructorLabel">Add Instructor</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <form method="post" action="">
+                <div class="mb-3">
+                  <label for="instructorName" class="form-label">Name</label>
+                  <input type="text" class="form-control" id="instructorName" aria-describedby="nameHelp" name="iName">
+                  <div id="nameHelp" class="form-text">Enter the instructor's name.</div>
+                </div>
+                <input type="hidden" name="saveType" value="Add">
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
 </body>
     
 <?php require_once("footer.php"); ?>
