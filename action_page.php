18ffa185635431e1642ca8a2042fdb87e51d9182
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-11-25 16:12:47 -0600

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -1,16 +1,16 @@
 <?php require_once("header.php"); ?>
 
 if <?php ($_POST["projects"] === 'biz')  {
- print "<h2>Business Projects:</h2>
+ print "<h2>Business Projects:</h2>";
 
-<ul>
+print "<ul>
   <li>Sweaters</li>
   <li>Waterbottle</li>
 </ul>"; ?>
 } elseif <?php ($_POST["projects"] === 'infosys')  {
- print "<h2>Information Systems Projects:</h2>
+ print "<h2>Information Systems Projects:</h2>";
 
-<ul>
+print "<ul>
   <li>Espian</li>
   <li>Cabins In Broken Bow</li>
   <li>Website</li>
