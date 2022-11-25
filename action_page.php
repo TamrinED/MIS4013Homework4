2f65b341670288f3b39ae1e95b360fbc2cd192eb
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-11-25 16:17:58 -0600

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -3,19 +3,19 @@
 if <?php ($_POST["projects"] === 'biz')  {
  print "<h2>Business Projects:</h2>";
 
-print "<ul>
-  <li>Sweaters</li>
-  <li>Waterbottle</li>
-</ul>"; ?>
+<ul>
+  print "<li>Sweaters</li>";
+  print "<li>Waterbottle</li>";
+</ul> ?>
 } elseif <?php ($_POST["projects"] === 'infosys')  {
  print "<h2>Information Systems Projects:</h2>";
 
-print "<ul>
-  <li>Espian</li>
-  <li>Cabins In Broken Bow</li>
-  <li>Website</li>
-  <li>Database Connection</li>
-</ul>"; ?>
+ <ul>
+  print "<li>Espian</li>;"
+  print "<li>Cabins In Broken Bow</li>";
+  print "<li>Website</li>;"
+  print "<li>Database Connection</li>";
+</ul> ?>
 }
 
 
