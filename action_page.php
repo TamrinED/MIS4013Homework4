034028aaeb7ede1050c4bcca50bbe44d4e5f17d1
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-11-25 14:02:16 -0600

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -1,5 +1,24 @@
+<?php require_once("header.php"); ?>
 
-  <?php require_once("header.php"); ?>
+if (projects == biz) {
+<h2>Business Projects:</h2>
+
+<ul>
+  <li>Sweaters</li>
+  <li>Waterbottle</li>
+</ul> 
+}
+
+if (projects == infosys) {
+<h2>Information Systems Projects:</h2>
+
+<ul>
+  <li>Espian</li>
+  <li>Cabins In Broken Bow</li>
+  <li>Website</li>
+  <li>Database Connection</li>
+</ul> 
+}
 
 
 <?php require_once("footer.php"); ?>
