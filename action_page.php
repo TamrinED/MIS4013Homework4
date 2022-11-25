d1464aa1d731fb560384febf24ccc68509c650a4
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-11-25 17:07:21 -0600

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -9,15 +9,16 @@ if (isset($_POST['projects']) == 'biz') {
   <li>Waterbottle</li>
 </ul>";
 } elseif (isset($_POST['projects']) == 'infosys') {
- echo "<h2>Information Systems Projects:</h2>";
+ echo "<h2>Information Systems Projects:</h2>
 
- echo "<ul>
+ <ul>
   <li>Espian</li>
   <li>Cabins In Broken Bow</li>
   <li>Website</li>
   <li>Database Connection</li>
 </ul>";
-}?>
+}
+?>
 
 
 <?php require_once("footer.php"); ?>
