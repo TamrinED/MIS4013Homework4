41be17461a5d9ae3fddde2bff49acc7fbdbd95b3
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-11-25 16:27:42 -0600

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -1,20 +1,20 @@
 <?php require_once("header.php"); ?>
 
-if <?php ($_POST["projects"] === 'biz')  Then {
+if <?php ($_POST["projects"] === 'biz') {
  echo "<h2>Business Projects:</h2>
 
 <ul>
-  echo "<li>Sweaters</li>";
-  echo "<li>Waterbottle</li>";
+  <li>Sweaters</li>
+  <li>Waterbottle</li>
 </ul>"; ?>
-} elseif <?php ($_POST["projects"] === 'infosys') Then {
+} elseif <?php ($_POST["projects"] === 'infosys') {
  echo "<h2>Information Systems Projects:</h2>
 
  <ul>
-  echo "<li>Espian</li>;"
-  echo "<li>Cabins In Broken Bow</li>";
-  echo "<li>Website</li>;"
-  echo "<li>Database Connection</li>";
+  <li>Espian</li>
+  <li>Cabins In Broken Bow</li>
+  <li>Website</li>
+  <li>Database Connection</li>
 </ul>"; ?>
 }
 
