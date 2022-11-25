1280417ac8546a4f4cf73db503841b0f572d4b32
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-11-25 16:56:04 -0600

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -2,21 +2,21 @@
 
 <?php 
 if (isset($_POST['projects']) == 'biz') {
- echo "<h2>Business Projects:</h2>
+ echo "<h2>Business Projects:</h2>";
 
 <ul>
-  <li>Sweaters</li>
-  <li>Waterbottle</li>
-</ul>";
+  echo "<li>Sweaters</li>";
+  echo "<li>Waterbottle</li>";
+</ul>
 } elseif (isset($_POST['projects']) == 'infosys') {
- echo "<h2>Information Systems Projects:</h2>
+ echo "<h2>Information Systems Projects:</h2>";
 
  <ul>
-  <li>Espian</li>
-  <li>Cabins In Broken Bow</li>
-  <li>Website</li>
-  <li>Database Connection</li>
-</ul>"; 
+  echo "<li>Espian</li>";
+  echo "<li>Cabins In Broken Bow</li>";
+  echo "<li>Website</li>";
+  echo" <li>Database Connection</li>";
+</ul>
 }?>
 
 
