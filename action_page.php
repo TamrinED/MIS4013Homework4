99ea03faeac2df0b00d56a51b01715890c6af118
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-11-25 15:11:53 -0600

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -2,14 +2,14 @@
 
 <?php
 
-if ($_POST[''] === 'biz') {
+if ($_POST['projects'] === 'biz') {
   echo "<h2>Business Projects:</h2>
 
 <ul>
   <li>Sweaters</li>
   <li>Waterbottle</li>
 </ul>";
-} elseif ($_POST[''] === 'infosys') {
+} elseif ($_POST['projects'] === 'infosys') {
   echo "<h2>Information Systems Projects:</h2>
 
 <ul>
